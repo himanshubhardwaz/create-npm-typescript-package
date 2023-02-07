77c3960dd17bc3e623b081ca8da630877d545d3c
Himanshu Bhardwaz <himanshu76200@gmail.com>
2023-02-08 00:40:40 +0530

    subtract function

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ export const add = (a: number, b: number) => {
   return a + b;
 };
 
-//export const subtract = (a: number, b: number) => {
-//  return a - b;
-//};
+export const subtract = (a: number, b: number) => {
+  return a - b;
+};
 
 //export const multiply = (a: number, b: number) => {
 //  return a * b;
